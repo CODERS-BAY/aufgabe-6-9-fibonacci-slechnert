10c45dd98769128612e03169efd12148744a2881
Simon <s.lechner.biz@gmail.com>
2020-02-24 14:51:18 +0100

    ez life

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -1,9 +1,27 @@
 package com.codersbay;
 
+import java.util.Scanner;
+
+
 public class Fibonacci {
 
     public static void main(String[] args) {
         System.out.println("Hello Fibonacci");
+        Scanner sc = new Scanner(System.in);
+        Integer stopp = sc.nextInt();
+        System.out.println(calcFib(stopp));
     }
 
+    private static Integer calcFib(Integer stopp) {
+        if (stopp == 0 || stopp == 1) {
+            return stopp;
+        } else {
+            return calcFib(stopp - 1) + calcFib(stopp - 2);
+        }
+    }
+
+
 }
+
+
+
